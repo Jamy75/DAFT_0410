632894de51e9017716c5fdc477956396868a6632
Jamy75 <126635792+Jamy75@users.noreply.github.com>
2023-04-24 02:05:04 +0200

    Add files via upload

---
+++ Module_1/Lab_20_SQL-Business-Cases/LAB 20 challange 2.sql
@@ -0,0 +1,30 @@
+
+/*
+CREATE DATABASE lab_mysql:
+USE test;
+CREATE TABLE Persons (
+PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255) 
+);
+*/
+/*
+				CREATE TABLE invoice (
+			Invoice_number INT,
+            Date DATE,
+			Car VARCHAR(255),
+			Customer VARCHAR(255),
+			Salesperson VARCHAR(255)
+            );
+  
+  */
+
+							CREATE TABLE Salesperson (
+								Staff_ID INT,
+								Name VARCHAR(255),
+								Store VARCHAR(255)
+								);
+          
+            
