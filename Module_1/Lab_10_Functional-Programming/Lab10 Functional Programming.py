4125727fde1e62f86f504e7fa49f3355aaec713e
Jamy75 <126635792+Jamy75@users.noreply.github.com>
2023-05-04 19:04:46 +0200

    Add files via upload

---
+++ Module_1/Lab_10_Functional-Programming/Lab10 Functional Programming.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def count_sheeps(sheeps):
+    return sum(sheeps)
+
+
+# In[ ]:
+
+
+def remove_every_other(my_list):
+    return my_list[::2]
+
+
+# In[ ]:
+
+
+def digitize(n):
+    return [int(digit) for digit in str(n)][::-1]
+
+
+# In[ ]:
+
+
+
+
